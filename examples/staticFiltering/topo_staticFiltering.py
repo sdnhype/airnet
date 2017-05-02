74ce9f8c1c0d6e9fdc4628f19553eb43c0fd7457
Mayoro BADJI <mayoro.badji@univ-tlse3.fr>
2017-05-02 17:15:55 +0200

    Tested staticFiltering example with OF13

--- examples/staticFiltering/topo_staticFiltering.py
+++ examples/staticFiltering/topo_staticFiltering.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python
+from mininet.net import Mininet
+from mininet.node import Controller, Node, RemoteController, OVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.util import waitListening
+from functools import partial
 import pdb
 import sys
 
@@ -8,26 +14,19 @@ import sys
    inet_h -- s1              s9 -- ssh_gw
                \            /
                s2 -- s3 -- s5 -- s7 -- priv_net_h
-                 \        /  
+                 \        /
                   s4 -- s6 -- s8
                              /   \
-                       wifipub_h   wifipriv_h    
+                       wifipub_h   wifipriv_h
 
 """
-
-from mininet.net import Mininet
-from mininet.node import Controller, Node, RemoteController
-from mininet.cli import CLI
-from mininet.log import setLogLevel, info
-
-from mininet.util import waitListening
-
 
 def emptyNet(controller_ip, controller_port):
 
 
 	# "Create an empty network and add nodes to it."
-	net = Mininet( controller=Controller )
+    switch = partial( OVSSwitch, protocols='OpenFlow13' )
+	net = Mininet( controller=Controller ,switch=switch)
 
 	info( '*** Adding controller\n' )
 	net.addController( 'c0', controller=RemoteController, ip=controller_ip, port=controller_port )
@@ -39,7 +38,7 @@ def emptyNet(controller_ip, controller_port):
 	h_privnet = net.addHost( 'priv_net_h' , ip= '172.16.0.50/16', defaultRoute = "via 172.16.0.50")
 	h_wifipub = net.addHost( 'wifipub_h' , ip= '192.168.20.2/24', defaultRoute = "via 192.168.20.2")
 	h_wifipriv = net.addHost( 'wifipriv_h' , ip= '192.168.30.3/24', defaultRoute = "via 192.168.30.3")
-	
+
 	info( '*** Adding switch\n' )
 	s1 = net.addSwitch( 's1' )
 	s2 = net.addSwitch( 's2' )
@@ -67,20 +66,20 @@ def emptyNet(controller_ip, controller_port):
 	net.addLink(s7, h_privnet)
 	net.addLink(s9, h_ws)
 	net.addLink(s9, h_ssh)
-	
+
 	info( '*** Starting network\n')
 	net.start()
 
 	print "*** Starting ssh service on ssh_gw"
 	h_ssh.cmd( '/usr/sbin/sshd -D -o UseDNS=no -u0 &' )
 	print "*** Starting web service on ws"
-        h_ws.cmd( 'python -m SimpleHTTPServer 80 &' )	
+        h_ws.cmd( 'python -m SimpleHTTPServer 80 &' )
 
-	net.pingAll(timeout=1)	
+	net.pingAll(timeout=1)
 
 	info( '*** Running CLI\n' )
 	CLI( net )
-	
+
 	info( '*** Stopping network' )
 	net.stop()
 
@@ -90,4 +89,3 @@ if __name__ == '__main__':
 		print 'Usage: ', sys.argv[0], ' controller_ip controller_port'
 		sys.exit()
 	emptyNet(sys.argv[1], int(sys.argv[2]))
-
