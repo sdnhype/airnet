e4b06915094d971fe33629a634c78e873daacf79
Emmanuel Lavinal <Emmanuel.Lavinal@univ-tlse3.fr>
2017-05-04 01:41:29 +0200

    staticParallelCatch update

--- examples/staticParallelCatch/catchComposition.py
+++ examples/staticParallelCatch/catchComposition.py
@@ -8,7 +8,7 @@ from language import *
 staff_net-|---|
 ----------    |
               |---[users_IO]----|
-----------    |                 |                                
+----------    |                 |
 guests_net-|--|                 |                                   |--- WS1
 ----------                      |              |---[users_egress]---|
                                 |---[fabric]---|                    |--- WS2
@@ -62,42 +62,41 @@ def users_IO_policy(VID):
 def admins_IO_policy(VID):
     i1 = match(edge=VID, src="admins_net", dst="DB") >> tag("admins_in_flows") >> forward("fabric")
     i2 = match(edge=VID, dst="admins_net") >> forward("admins_net")
-    return i1 + i2 
+    return i1 + i2
 
 def users_egress_policy(VID):
     i1 = match(edge=VID, dst="WS1") >> forward("WS1")
     i2 = match(edge=VID, dst="WS2") >> forward("WS2")
     i3 = match(edge=VID, dst="staff_net") >> tag("staff_out_flows") >> forward("fabric")
     i4 = match(edge=VID, dst="guests_net") >> tag("guests_out_flows") >> forward("fabric")
-    return i1 + i2 + i3 + i4 
+    return i1 + i2 + i3 + i4
 
 def admins_egress_policy(VID):
     i1 = match(edge=VID, dst="DB") >> forward("DB")
     i2 = match(edge=VID, dst="admins_net") >> tag("admins_out_flows") >> forward("fabric")
-    return i1 + i2 
+    return i1 + i2
 
 def fabric_policy():
-    
+
     # ( catch() + catch() ) >> carry
 
-    t1 = (catch(fabric="fabric", src="users_IO", flow="staff_in_flows") + 
-            catch(fabric="fabric", src="users_IO", flow="guests_in_flows")) >> carry(dst="users_egress")
-            
-    t2 = (catch(fabric="fabric", src="users_egress", flow="staff_out_flows") + 
-            catch(fabric="fabric", src="users_egress", flow="guests_out_flows")) >> carry(dst="users_IO")
-            
+    t1 = (catch(fabric="fabric", src="users_IO", flow="staff_in_flows") +
+          catch(fabric="fabric", src="users_IO", flow="guests_in_flows")) >> carry(dst="users_egress")
+
+    t2 = (catch(fabric="fabric", src="users_egress", flow="staff_out_flows") +
+          catch(fabric="fabric", src="users_egress", flow="guests_out_flows")) >> carry(dst="users_IO")
+
     t3 = catch(fabric="fabric", src="admins_IO", flow="admins_in_flows") >> carry(dst="admins_egress")
-    
+
     t4 = catch(fabric="fabric", src="admins_egress", flow="admins_out_flows") >> carry(dst="admins_IO")
-    
+
     return t1 + t2 + t3 + t4
 
 def main():
-    in_network_functions = (users_IO_policy("users_IO") + admins_IO_policy("admins_IO") + 
+    in_network_functions = (users_IO_policy("users_IO") + admins_IO_policy("admins_IO") +
                             users_egress_policy("users_egress") + admins_egress_policy("admins_egress"))
     transport_function = fabric_policy()
-    topology = virtual_network() 
-    return {"virtual_topology": topology, 
-            "edge_policies": in_network_functions, 
-            "fabric_policies": transport_function}  
-
+    topology = virtual_network()
+    return {"virtual_topology": topology,
+            "edge_policies": in_network_functions,
+            "fabric_policies": transport_function}
--- examples/staticParallelCatch/catchComposition_topo.py
+++ examples/staticParallelCatch/catchComposition_topo.py
@@ -10,7 +10,7 @@ import sys
 staff_net-|---|
 ----------    |
               |---[users_IO]----|
-----------    |                 |                                
+----------    |                 |
 guests_net-|--|                 |                                   |--- WS1
 ----------                      |              |---[users_egress]---|
                                 |---[fabric]---|                    |--- WS2
@@ -40,77 +40,81 @@ admins_net---|---[s3]--|
 """
 
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController
+from mininet.node import Controller, RemoteController, OVSSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
+from functools import partial
 
-def emptyNet(controller_ip, controller_port):
+def defaultNet(controller_ip, controller_port):
 
-	"Create an empty network and add nodes to it."
+    # Create an empty network and add nodes to it.
+    switch = partial( OVSSwitch, protocols='OpenFlow13' )
+    net = Mininet( controller=Controller, switch=switch )
 
-	net = Mininet( controller=Controller )
+    info( '*** Adding controller\n' )
+    net.addController( 'c0', controller=RemoteController, ip=controller_ip, port=controller_port )
 
-	info( '*** Adding controller\n' )
-	net.addController( 'c0', controller=RemoteController, ip=controller_ip, port=controller_port )
+    info( '*** Adding hosts\n' )
+    staff_net = net.addHost( 'staff_net' , ip= '172.16.0.11/12', defaultRoute = 'via 172.16.0.11')
+    guests_net = net.addHost( 'guests_net' , ip= '192.168.0.11/16', defaultRoute = "via 192.168.0.11")
+    admins_net = net.addHost( 'admins_net' , ip= '10.0.0.11/16', defaultRoute = "via 10.0.0.11")
+    DB = net.addHost( 'DB', ip='141.115.28.11', defaultRoute='via 141.115.28.11')
+    WS1 = net.addHost( 'WS1' , ip= '141.115.28.12', defaultRoute = "via 141.115.28.12")
+    WS2 = net.addHost('WS2', ip='141.115.28.13', defaultRoute='via 141.115.28.13')
 
-	info( '*** Adding hosts\n' )
-	staff_net = net.addHost( 'staff_net' , ip= '172.16.0.11/12', defaultRoute = 'via 172.16.0.11')
-	guests_net = net.addHost( 'guests_net' , ip= '192.168.0.11/16', defaultRoute = "via 192.168.0.11")
-	admins_net = net.addHost( 'admins_net' , ip= '10.0.0.11/16', defaultRoute = "via 10.0.0.11")
-	DB = net.addHost( 'DB', ip='141.115.28.11', defaultRoute='via 141.115.28.11')
-	WS1 = net.addHost( 'WS1' , ip= '141.115.28.12', defaultRoute = "via 141.115.28.12")
-	WS2 = net.addHost('WS2', ip='141.115.28.13', defaultRoute='via 141.115.28.13')
-	
-	
-	info( '*** Adding switch\n' )
-	s1 = net.addSwitch( 's1' )
-	s2 = net.addSwitch( 's2' )
-	s3 = net.addSwitch( 's3' )
-	s4 = net.addSwitch( 's4' )
-	s5 = net.addSwitch( 's5' )
-	s6 = net.addSwitch( 's6' )
-	s7 = net.addSwitch( 's7' )
-	s8 = net.addSwitch( 's8' )
-	s9 = net.addSwitch( 's9' )
-	s10 = net.addSwitch( 's10') 
-	s11 = net.addSwitch( 's11' )
-	s12 = net.addSwitch( 's12' )
 
-	info( '*** Creating links\n' )
-	net.addLink( s1, s4)
-	net.addLink( s2, s4)
-	net.addLink( s3, s4)
-	net.addLink( s4, s5)
-	net.addLink( s5, s6)
-	net.addLink( s6, s7)
-	net.addLink( s4, s8)
-	net.addLink( s8, s9)
-	net.addLink( s9, s10)
-	net.addLink( s10, s7)
-	net.addLink( s7, s11)
-	net.addLink( s7, s12)
-	net.addLink(s11, WS1)
-	net.addLink(s11, WS2)
-	net.addLink(s12, DB)
-	net.addLink(s1, staff_net)
-	net.addLink(s2, guests_net)
-	net.addLink(s3, admins_net)	
-	
-	info( '*** Starting network\n')
-	net.start()
-	
-	net.pingAll(timeout=1)	
+    info( '*** Adding switch\n' )
+    s1 = net.addSwitch( 's1' )
+    s2 = net.addSwitch( 's2' )
+    s3 = net.addSwitch( 's3' )
+    s4 = net.addSwitch( 's4' )
+    s5 = net.addSwitch( 's5' )
+    s6 = net.addSwitch( 's6' )
+    s7 = net.addSwitch( 's7' )
+    s8 = net.addSwitch( 's8' )
+    s9 = net.addSwitch( 's9' )
+    s10 = net.addSwitch( 's10')
+    s11 = net.addSwitch( 's11' )
+    s12 = net.addSwitch( 's12' )
 
-	info( '*** Running CLI\n' )
-	CLI( net )
-	
-	info( '*** Stopping network' )
-	net.stop()
+    info( '*** Creating links\n' )
+    net.addLink( s1, s4)
+    net.addLink( s2, s4)
+    net.addLink( s3, s4)
+    net.addLink( s4, s5)
+    net.addLink( s5, s6)
+    net.addLink( s6, s7)
+    net.addLink( s4, s8)
+    net.addLink( s8, s9)
+    net.addLink( s9, s10)
+    net.addLink( s10, s7)
+    net.addLink( s7, s11)
+    net.addLink( s7, s12)
+    net.addLink(s11, WS1)
+    net.addLink(s11, WS2)
+    net.addLink(s12, DB)
+    net.addLink(s1, staff_net)
+    net.addLink(s2, guests_net)
+    net.addLink(s3, admins_net)
+
+    info( '*** Starting network\n')
+    net.start()
+
+    # pingAll takes too much time!
+    # net.pingAll(timeout=1)
+    net.ping([staff_net, guests_net], timeout=1)
+    net.ping([admins_net, DB], timeout=1)
+    net.ping([WS1, WS2], timeout=1)
+
+    info( '*** Running CLI\n' )
+    CLI( net )
+
+    info( '*** Stopping network' )
+    net.stop()
 
 if __name__ == '__main__':
-	setLogLevel( 'info' )
-	if len(sys.argv) != 3:
-		print 'Usage: ', sys.argv[0], ' controller_ip controller_port'
-		sys.exit()
-	emptyNet(sys.argv[1], int(sys.argv[2]))
-
+    setLogLevel( 'info' )
+    if len(sys.argv) != 3:
+        print 'Usage: ', sys.argv[0], ' controller_ip controller_port'
+        sys.exit()
+    defaultNet(sys.argv[1], int(sys.argv[2]))
